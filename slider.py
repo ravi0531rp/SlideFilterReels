aebcc5e86b6ee19d2362fb002c03e94f5235b63a
RAVI PRAKASH <ravi.0531.rp@gmail.com>
2021-03-12 13:33:12 +0530

    Update slider.py

--- slider.py
+++ slider.py
@@ -1,13 +1,26 @@
+"""
+@author : Ravi Prakash
+
+Created on Friday 12 12:54:53 2020
+
+"""
+
 import cv2
 import numpy as np 
+import datetime
 cap = cv2.VideoCapture(0)
 ret,init_frame = cap.read()
 init_frame = cv2.flip(init_frame,1)
 
 h,w,_ = init_frame.shape
-treadLength = 5
+treadLength = 2
 start = w
 
+curr = str(datetime.datetime.now()).replace(":","_").replace(".","_").replace(" ","__")
+
+result = cv2.VideoWriter('Slider_' + curr + '.avi' ,  
+                         cv2.VideoWriter_fourcc(*'MJPG'), 
+                         20, (w,h)) 
 
 while True:
 	ret,frame = cap.read()
@@ -18,6 +31,8 @@ while True:
 		init_frame[: , start - treadLength : start , :] = frame[: , start - treadLength : start , :]
 		temp_init = init_frame.copy()
 		temp_init = cv2.line(temp_init , (start,0) , (start,h) , (0,255, 0) , 4)
+		temp_init = cv2.GaussianBlur(temp_init , (9,9) ,0)
+		result.write(temp_init)
 		cv2.imshow("frame",temp_init)
 
 		if start <=0:
@@ -26,4 +41,5 @@ while True:
 			break 
 
 cap.release()
+result.release()
 cv2.destroyAllWindows()
