78d7d80a2b551348ed59656a9129c3ac29cc563d
RAVI PRAKASH <ravi.0531.rp@gmail.com>
2021-03-13 18:23:25 +0530

    Update slider.py

--- slider.py
+++ slider.py
@@ -3,49 +3,70 @@
 Created on Friday 12 12:54:53 2020
 """
 
-import cv2
-import numpy as np 
-import datetime
-cap = cv2.VideoCapture(0)
-ret,init_frame = cap.read()
-init_frame = cv2.flip(init_frame,1)
+from playsound import playsound
+from multiprocessing import Process
+import cv2 
+import numpy as np  
+import datetime   
 
-h,w,_ = init_frame.shape
-treadLength = 2
-start = w
+def musica():
+	playsound('astronout.mp3')
 
-curr = str(datetime.datetime.now()).replace(":","_").replace(".","_").replace(" ","__")
 
-result = cv2.VideoWriter('Slider_' + curr + '.avi' ,  
-                         cv2.VideoWriter_fourcc(*'MJPG'), 
-                         20, (w,h)) 
+def slider():
+	cap = cv2.VideoCapture(0)
+	ret,init_frame = cap.read()
+	init_frame = cv2.flip(init_frame,1)
 
-prev_frame = init_frame.copy()
-while True:
-	if start - treadLength <=0:
-			break
-	ret,frame = cap.read()
-	if ret:
-		frame = cv2.flip(frame,1)
-		frame = cv2.line(frame , (start,0) , (start,h) , (0,255, 0) , 4)
-		start = start - treadLength
-		init_frame[: , start - treadLength : start , :] = frame[: , start - treadLength : start , :]
-		init_frame[: , : start - treadLength , : ] = prev_frame[: , : start - treadLength , :]
-		prev_frame = frame.copy()
+	h,w,_ = init_frame.shape
+	treadLength = 2
+	start = w
 
-		temp_init = init_frame.copy()
-		temp_init = cv2.line(temp_init , (start,0) , (start,h) , (0,255, 0) , 4)
-		
-		temp_init = cv2.GaussianBlur(temp_init , (9,9) ,0)
-		result.write(temp_init)
-		cv2.imshow("frame",temp_init)
+	curr = str(datetime.datetime.now()).replace(":","_").replace(".","_").replace(" ","__")
 
-		
-		if cv2.waitKey(1) & 0xFF == ord('q'):
-			break 
+	result = cv2.VideoWriter('Slider_' + curr + '.avi' ,  
+	                         cv2.VideoWriter_fourcc(*'MJPG'), 
+	                         20, (w,h)) 
 
-cap.release()
-result.release()
-cv2.destroyAllWindows()
+	prev_frame = init_frame.copy()
+	while True:
+		if start - treadLength <=0:
+				break
+		ret,frame = cap.read()
+		if ret:
+			frame = cv2.flip(frame,1)
+			frame = cv2.line(frame , (start,0) , (start,h) , (0,255, 0) , 4)
+			start = start - treadLength
+			init_frame[: , start - treadLength : start , :] = frame[: , start - treadLength : start , :]
+			init_frame[: , : start - treadLength , : ] = prev_frame[: , : start - treadLength , :]
+			prev_frame = frame.copy()
 
-cv2.imwrite('Slider_' + curr +'.jpg' , temp_init)
+			temp_init = init_frame.copy()
+
+			saver = temp_init.copy()  # this is used to get the final image without any lines
+			temp_init = cv2.line(temp_init , (start,0) , (start,h) , (0,255, 0) , 4)
+			
+			temp_init = cv2.GaussianBlur(temp_init , (9,9) ,0)
+			saver = cv2.GaussianBlur(saver , (9,9) ,0)
+			result.write(temp_init)
+			cv2.imshow("frame",temp_init)
+
+			
+			if cv2.waitKey(1) & 0xFF == ord('q'):
+				break 
+
+	cap.release()
+	result.release()
+	cv2.destroyAllWindows()
+
+	cv2.imwrite('Slider_' + curr +'.jpg' , saver)
+
+if __name__ == '__main__':
+  p1 = Process(target=musica)
+  p1.start()
+  p2 = Process(target=slider)
+  p2.start()
+  p1.join()
+  p2.join()
+
+
