9913d4b7c951dca8c9c694766c65e77479d6e3c7
RAVI PRAKASH <ravi.0531.rp@gmail.com>
2021-11-22 16:55:03 +0530

    Add files via upload

---
+++ procTest.py
@@ -0,0 +1,56 @@
+from multiprocessing import Process,Queue
+from datetime import datetime
+import time
+
+
+class Streamer:
+	def __init__(self, outputQueue):
+		self.outputQueue = outputQueue
+		self.end = False
+		self.iteration = 0
+	def start(self):
+		Process(target = self.stream , args = (self.outputQueue,)).start()
+
+	def stream(self, outQueue):
+		while not self.end:
+			self.iteration += 1
+			try:
+				outQueue.put([self.iteration , datetime.now()])
+				time.sleep(2)
+			except:
+				print("Exception occured while streaming")
+			
+
+	def stop(self):
+		self.end = True
+		print("Stopped streaming..")
+
+class StreamIngestor:
+	def __init__(self, inputQueue):
+		self.inputQueue = inputQueue
+		self.end = False
+		self.ingestIter = 0
+	def start(self):
+		Process(target = self.ingest , args = (self.inputQueue,)).start()
+
+	def ingest(self, inputQueue):
+		while not self.end:
+			self.ingestIter += 1
+			try:
+				streamIter , timeData = inputQueue.get_nowait()
+				print(f" streamIter : {streamIter} , ingestIter : {self.ingestIter} , timeData : {timeData} ")
+				time.sleep(1)
+			except:
+				# print("Problem while reading..")
+				pass
+
+	def stop(self):
+		self.end = True
+		print("Stopped Reading")
+
+
+if __name__ == "__main__":
+	commonQueue = Queue()
+	streamer = Streamer(commonQueue).start()
+	ingestor = StreamIngestor(commonQueue).start()
+
